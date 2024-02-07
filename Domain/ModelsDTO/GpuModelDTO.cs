fb6af3bf5d6463c887ad5ebd3f34e55944236ba8
Виталий Пахомович <warfire122@gmail.com>
2024-02-07 17:51:51 +0800

    Добавил две дтошки на GPU и Vender

---
+++ Domain/ModelsDTO/GpuModelDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArcProject.Domain.ModelsDTO
+{
+    internal class GpuModelDTO
+    {
+        public float Frequency { get; set; }
+
+        public Guid VenderId { get; set; }
+    }
+}
---
+++ Domain/ModelsDTO/VenderModelDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArcProject.Domain.ModelsDTO
+{
+    internal class VenderModelDTO
+    {
+        public string? Name { get; set; }
+
+        public float MarketShare { get; set; }
+    }
+}
